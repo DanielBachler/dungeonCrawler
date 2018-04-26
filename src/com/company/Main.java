559a94523fc9d6ca037df93d3c1d8ec72682086b
Daniel Bachler <daniel.bachler@comcast.net>
2018-04-26 10:43:47 -0600

    Laying initial framework.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+/**
+ TODO:
+        Main window: implement basics, with blank start screen and start button.  Upon press game begins
+        Map: Decide how map will be internally handled, then implement
+        Player: ability to move throughout map, and skills
+        Monsters: decide what kind of monsters and how many, implement
+        Figure out rest of TODO
+ */
+
 public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static void play() {
+        System.out.println("");
     }
 }
--- src/com/company/MainWindow.java
+++ src/com/company/MainWindow.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainWindow {
+    private JButton startButton;
+    private JLabel Map;
+
+    public MainWindow() {
+
+        //The start button listener
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
